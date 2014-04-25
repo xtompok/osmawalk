05a2d4ed3d136f2af6542983e55a7123f373a751
Tomas "Jethro" POkorny <xtompok@gmail.com>
2014-04-25 23:08:47 +0200

    Added code to calculate point in polygon

--- compiled/filter.c
+++ compiled/filter.c
@@ -1173,3 +1173,15 @@ int main (int argc, char ** argv){
 	
 	return 0;
 }
+/*
+int pnpoly(int nvert, float *vertx, float *verty, float testx, float testy)
+{
+  int i, j, c = 0;
+  for (i = 0, j = nvert-1; i < nvert; j = i++) {
+    if ( ((verty[i]>testy) != (verty[j]>testy)) &&
+     (testx < (vertx[j]-vertx[i]) * (testy-verty[i]) / (verty[j]-verty[i]) + vertx[i]) )
+       c = !c;
+  }
+  return c;
+}
+*/
