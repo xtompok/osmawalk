6eefcff3ca3a57988511932c0a59e93919d23007
Tomas "Jethro" POkorny <xtompok@gmail.com>
2014-05-07 23:33:01 +0200

    Added setting bbox with variables.

--- osm/prepare.sh
+++ osm/prepare.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
+MINLAT=50.101
+MINLON=14.40
+MAXLAT=50.111
+MAXLON=14.44
 
 posledni=`curl "http://osm.kyblsoft.cz/archiv/last_dates.txt" | grep gz | cut -d"	" -f2`
-echo "Posledni data z $posledni, stahnout?"
+echo "Posledni data z $posledni, stahuji..."
 
-curl "http://osm.kyblsoft.cz/archiv/czech_republic-$posledni.osm.gz" gunzip  > czech_republic.osm
+curl "http://osm.kyblsoft.cz/archiv/czech_republic-${posledni}.osm.gz" | gunzip > czech_republic.osm
 
-#./osmconvert czech_republic.osm -b=14.4,50.05,14.5,50.10 >praha.osm
-./osmconvert czech_republic.osm -b=14.224436,49.941901,14.706787,50.17743 >praha.osm
+./osmconvert czech_republic.osm -b=$MINLON,$MINLAT,$MAXLON,$MAXLAT >praha.osm
+#./osmconvert czech_republic.osm -b=14.224436,49.941901,14.706787,50.17743 >praha.osm
 
