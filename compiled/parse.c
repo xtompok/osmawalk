90ef1efe3e5f19d633d1b6498231774997218729
Tomas "Jethro" POkorny <xtompok@gmail.com>
2014-10-02 23:45:20 +0200

    Parsing OSM ready.
    Parser of config separated to yamlconf.h
    Parsing tunnels, areas and bridges works.
    Parsing heights works.

--- compiled/parse.c
+++ compiled/parse.c
@@ -13,6 +13,7 @@
 #include "include/types.pb-c.h"
 #include "include/premap.pb-c.h"
 #include "parse.h"
+#include "yamlconf.h"
 
 // TODO
 // - tunely, mosty, plochy
@@ -34,191 +35,63 @@ FILE * wayFile;
 FILE * nodeFile;
 FILE * mpFile;
 
+struct height_map_t heights;
 
-int enumValueForName(ProtobufCEnumDescriptor desc,char * name){
-	for (int i=0;i<desc.n_value_names;i++){
-		if (strcmp(name,desc.values_by_name[i].name)==0){
-			return desc.values[desc.values_by_name[i].index].value;
-		}	
-	}
-	return -1;
-};
-
-
-
-struct tag_t *  addItemToTagList(struct tag_t * list,struct mapConfItem_t item, int type){
-	printf("%s: %s -> %d (priority %d)\n",item.key,item.value,type,item.priority);
-	for (int i=0;i<GARY_SIZE(list);i++){
-		if (strcmp(list[i].key,item.key))
-			continue;
-		struct value_t * val;
-		val = GARY_PUSH(list[i].values);
-		val->prio = item.priority;
-		val->objtype = type;
-		val->name = malloc((strlen(item.value)+1)*sizeof(char));
-		strcpy(val->name,item.value);
-		return list;
-
-	}
-	printf("Creating new key: ");
-	struct tag_t * tag;
-	tag=GARY_PUSH(list);
-	tag->key = malloc((strlen(item.key)+1)*sizeof(char));
-	strcpy(tag->key,item.key);
-	printf("%s\n",tag->key);
-	GARY_INIT(tag->values,1);
-	struct value_t * val;
-	val = &tag->values[0];
-	val->prio = item.priority;
-	val->objtype = type;
-	val->name = malloc((strlen(item.value)+1)*sizeof(char));
-	strcpy(val->name,item.value);
-	return list;
-}
-void addAreaItemToMapConf(struct mapconfig_t *  conf,struct mapConfItem_t item){
-	int type;
-	if (strcmp(item.name,"yes")==0)
-		type = 1;
-	else if (strcmp(item.name,"no")==0)
-		type = 0;
-	else{
-		printf("Wrong value: %s\n",item.name);
-		return;
-	}
-	conf->area = addItemToTagList(conf->area,item,type);
-	return;
-}
-
-void addTunnelItemToMapConf(struct mapconfig_t *  conf,struct mapConfItem_t item){
-	int type;
-	if (strcmp(item.name,"yes")==0)
-		type = 1;
-	else if (strcmp(item.name,"no")==0)
-		type = 0;
-	else{
-		printf("Wrong value: %s\n",item.name);
-		return;
-	}
-	conf->tunnel = addItemToTagList(conf->tunnel,item,type);
-	return;
-}
-void addBridgeItemToMapConf(struct mapconfig_t *  conf,struct mapConfItem_t item){
-	int type;
-	if (strcmp(item.name,"yes")==0)
-		type = 1;
-	else if (strcmp(item.name,"no")==0)
-		type = 0;
-	else{
-		printf("Wrong value: %s\n",item.name);
-		return;
-	}
-	conf->bridge = addItemToTagList(conf->bridge,item,type);
-	return;
-}
-
-void addTypeItemToMapConf(struct mapconfig_t *  conf,struct mapConfItem_t item){
-	int enum_val;
-	enum_val = enumValueForName(conf->desc, item.name);
-	conf->type = addItemToTagList(conf->type,item,enum_val);
-}
-
-int parseMapConfigFile(char * filename, struct mapconfig_t * conf,
-			void (*addItemToMapConf)(struct mapconfig_t * conf,struct mapConfItem_t item )){
-
-	yaml_parser_t parser;
-	yaml_document_t document;
-
-	yaml_parser_initialize(&parser);
-
+struct height_map_t loadHeights(char * filename){
+	long len;
+	struct height_map_t map;
 	FILE * IN;
 	IN = fopen(filename,"r");
-	if (IN==NULL){
-		printf("Config file opening error\n");	
-		return 1;
+	if (IN == NULL){
+		printf("File %s not found", filename);
+		return map;
 	}
-	yaml_parser_set_input_file(&parser,IN);
-
-	yaml_parser_load(&parser,&document);
-	fclose(IN);
-
-	yaml_node_t * root;
-	yaml_node_t * anode; // use as temporary storage
-	struct mapConfItem_t mapConfItem;
-
-	root = yaml_document_get_root_node(&document);
-
-	if (root->type != YAML_MAPPING_NODE){
-		printf("Wrong syntax of configuration file in objtypes\n");
-		return 2;
-	}
-	for (yaml_node_pair_t * type=root->data.mapping.pairs.start;
-			type < root->data.mapping.pairs.top; type++){
-		anode = yaml_document_get_node(&document,type->key);
-		mapConfItem.name = anode->data.scalar.value;
-		printf("%s\n",mapConfItem.name);
-
-		yaml_node_t * priomap;
-		priomap = yaml_document_get_node(&document,type->value);
-		if (priomap->type != YAML_MAPPING_NODE){
-			printf("Wrong syntax of configuration file in priorities\n");
-			return 3;
-		}
-		for (yaml_node_pair_t * prio=priomap->data.mapping.pairs.start;
-				prio < priomap->data.mapping.pairs.top; prio++){
-			anode = yaml_document_get_node(&document,prio->key);
-			mapConfItem.priority=atoi(anode->data.scalar.value);
-			printf("	%d\n",mapConfItem.priority);
-			yaml_node_t * tagmap;
-			tagmap = yaml_document_get_node(&document,prio->value);
-			if (tagmap->type != YAML_MAPPING_NODE){
-				printf("Wrong syntax of configuration file in tags\n");
-				return 4;
-			}
-			for (yaml_node_pair_t * atag=tagmap->data.mapping.pairs.start;
-					atag < tagmap->data.mapping.pairs.top; atag++){
-				anode = yaml_document_get_node(&document,atag->key);
-				mapConfItem.key = anode->data.scalar.value;
-				printf("		%s\n",mapConfItem.key);
-				yaml_node_t * vallist;
-				vallist = yaml_document_get_node(&document,atag->value);
-				if (vallist->type == YAML_SEQUENCE_NODE){
-					for (yaml_node_item_t *  val=vallist->data.sequence.items.start;
-							val < vallist->data.sequence.items.top; val++){
-						anode = yaml_document_get_node(&document,*val);
-						mapConfItem.value=anode->data.scalar.value;
-						printf("			%s\n",mapConfItem.value);
-						addItemToMapConf(conf,mapConfItem);
-					}
-				} else if (vallist->type == YAML_SCALAR_NODE){ 
-					mapConfItem.value=vallist->data.scalar.value;
-					printf("			%s\n",mapConfItem.value);
-					
-					addItemToMapConf(conf,mapConfItem);
-				}else {
-					printf("Wrong syntax of configuration file in values\n");
-					return 5;
-				}
-			}
-		}
-	}
+	fseek(IN,0,SEEK_END);
+	len = ftell(IN);
+	rewind(IN);
+	fread(&map.minlon,1,sizeof(int),IN);
+	fread(&map.minlat,1,sizeof(int),IN);
+	fread(&map.maxlon,1,sizeof(int),IN);
+	fread(&map.maxlat,1,sizeof(int),IN);
 	
-	yaml_document_delete(&document);		
-	yaml_parser_delete(&parser);
+	printf("Heights from N %d E %d to N %d E %d\n",map.minlat,map.minlon,map.maxlat,map.maxlon);
 
-	return 0;
+	map.map = malloc(sizeof(int)*(len/2));
+	for (int i=0;i<len/2;i++){
+		map.map[i]=0;
+		fread(map.map+i,1,2,IN);
+	}
+	return map;
 }
 
-int classify(OSM_Tag_List * tags){
+int calcHeight(struct height_map_t map, double lat, double lon){
+	if ((lat < map.minlat) || (lat >= map.maxlat))
+		return 0;
+	if ((lon < map.minlon) || (lon >= map.maxlon))
+		return 0;
+	lat -= map.minlat;
+	lon -= map.minlon;
+	lat *=1200;
+	lon *=1200;
+	int rowlen;
+	int coord;
+	rowlen = (map.maxlon-map.minlon)*1200;
+	coord = ((int)lat)*rowlen+((int)lon);
+	return map.map[coord];
+}
+
+
+int classify(OSM_Tag_List * tags,struct tag_t * classifier){
 	int priority = -1;
 	int objtype = 0;
 	for (int i=0;i<tags->num;i++){
 		OSM_Tag tag;
 		tag = tags->data[i];
-		for (int k=0;k<GARY_SIZE(conf.type);k++){
-			if (strcmp(conf.type[k].key,tag.key))
+		for (int k=0;k<GARY_SIZE(classifier);k++){
+			if (strcmp(classifier[k].key,tag.key))
 				continue;
 			struct tag_t tag;
-			tag = conf.type[k];
+			tag = classifier[k];
 			for (int v=0;v<GARY_SIZE(tag.values);v++){
 				if ((tag.values[v].name[0]=='*') && 
 				    (tag.values[v].prio>priority)){
@@ -238,13 +111,17 @@ int classify(OSM_Tag_List * tags){
 	return objtype;
 }
 
-void dumpMultipol(OSM_Relation *mp, int objtype){
+void dumpMultipol(OSM_Relation *mp, struct obj_attr attr){
 	Premap__Multipolygon * pbMp;
 	pbMp = malloc(sizeof(Premap__Multipolygon));
 	premap__multipolygon__init(pbMp);
 	pbMp->id = mp->id;
 	pbMp->has_type = true;
-	pbMp->type = objtype;
+	pbMp->type = attr.objtype;
+//	pbMp->has_tunnel = true;
+//	pbMp->tunnel = attr.tunnel;
+//	pbMp->has_bridge = true;
+//	pbMp->bridge = attr.bridge;
 
 	// Write refs etc.
 	pbMp->n_refs = mp->member->num;
@@ -289,13 +166,19 @@ void dumpMultipol(OSM_Relation *mp, int objtype){
 	free(pbMp);
 
 }
-void dumpWay(OSM_Way * way, int objtype){
+void dumpWay(OSM_Way * way, struct obj_attr attr){
 	Premap__Way * pbWay;
 	pbWay = malloc(sizeof(Premap__Way));
 	premap__way__init(pbWay);
 	pbWay->id = way->id;
 	pbWay->has_type = true;
-	pbWay->type = objtype;
+	pbWay->type = attr.objtype;
+	pbWay->has_tunnel = true;
+	pbWay->tunnel = attr.tunnel;
+	pbWay->has_bridge = true;
+	pbWay->bridge = attr.bridge;
+	pbWay->has_area = true;
+	pbWay->area = attr.area;
 	int n_refs;
 	n_refs = 0;
 	while (way->nodes[n_refs]!=0)
@@ -317,13 +200,19 @@ void dumpWay(OSM_Way * way, int objtype){
 
 }
 
-void dumpNode(OSM_Node * node, int objtype){
+void dumpNode(OSM_Node * node, struct obj_attr attr){
 	Premap__Node * pbNode;
 	pbNode = malloc(sizeof(Premap__Node));
 	premap__node__init(pbNode);
 	pbNode->id = node->id;
 	pbNode->has_objtype = true;
-	pbNode->objtype = objtype;
+	pbNode->objtype = attr.objtype;
+	pbNode->has_intunnel = true;
+	pbNode->intunnel = attr.tunnel;
+	pbNode->has_onbridge = true;
+	pbNode->onbridge = attr.bridge;
+	pbNode->has_height = true;
+	pbNode->height = calcHeight(heights,node->lat,node->lon);
 	pbNode->lat = node->lat;
 	pbNode->lon = node->lon;
 	size_t len;
@@ -348,24 +237,34 @@ int relation(OSM_Relation *r) {
 			return 0;
 		printf("	%s: %s\n",r->tags->data[i].key,r->tags->data[i].val);
     	}
-	int objtype = classify(r->tags);
-	dumpMultipol(r,objtype);
+	struct obj_attr attr;
+	attr.objtype = classify(r->tags,conf.type);
+	attr.tunnel  = classify(r->tags,conf.tunnel);
+	attr.bridge = classify(r->tags,conf.bridge);
+	dumpMultipol(r,attr);
     	return 0;
 }
 
 int way(OSM_Way *w) {
-	int objtype = classify(w->tags);
+	struct obj_attr attr;
+	attr.objtype = classify(w->tags,conf.type);
+	attr.area = classify(w->tags,conf.area);
+	attr.tunnel  = classify(w->tags,conf.tunnel);
+	attr.bridge = classify(w->tags,conf.bridge);
 //	if (objtype==-1)
 //		return;
-	dumpWay(w,objtype);
+	dumpWay(w,attr);
 	return 0;
 }
 
 int node(OSM_Node *n) {
-	int objtype = classify(n->tags);
+	struct obj_attr attr;
+	attr.objtype = classify(n->tags,conf.type);
+	attr.tunnel  = classify(n->tags,conf.tunnel);
+	attr.bridge = classify(n->tags,conf.bridge);
 //	if (objtype==-1)
 //		return;
-	dumpNode(n,objtype);
+	dumpNode(n,attr);
 	return 0;
 }
 
@@ -386,6 +285,10 @@ int main(int argc, char **argv) {
 	parseMapConfigFile("../config/area.yaml",&conf,addAreaItemToMapConf);
 	parseMapConfigFile("../config/bridge.yaml",&conf,addBridgeItemToMapConf);
 	parseMapConfigFile("../config/tunnel.yaml",&conf,addTunnelItemToMapConf);
+
+	heights = loadHeights("../osm/heights.bin");
+
+
 
 	wayFile = fopen("../data/ways-stage1","w");
 	nodeFile = fopen("../data/nodes-stage1","w");
--- compiled/parse.h
+++ compiled/parse.h
@@ -1,26 +1,16 @@
+struct obj_attr {
+	int objtype;
+	int area;
+	int tunnel;
+	int bridge;
+	int height;
 
-struct value_t {
-	char * name;
-	int prio;
-	int objtype;	
 };
 
-struct tag_t {
-	char * key;
-	struct value_t * values;
-};	
-
-struct mapconfig_t {
-	ProtobufCEnumDescriptor desc;
-	struct tag_t * type;
-	struct tag_t * tunnel;
-	struct tag_t * bridge;
-	struct tag_t * area;
-};
-
-struct mapConfItem_t {
-	char * key;
-	char * value;
-	int priority;
-	char * name;
+struct height_map_t {
+	int minlon;
+	int minlat;
+	int maxlon;
+	int maxlat;
+	int * map;
 };
