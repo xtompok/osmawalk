cc666a16f88b002517a0946701162b844119a38a
Tomas "Jethro" Pokorny <xtompok@gmail.com>
2014-11-22 16:07:01 +0100

    Added postgis routines for preparing data.

---
+++ postgis/duplicate_nodes.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE dedup_nodes AS 
+SELECT n1.lat AS lat, n1.lon AS lon, MIN(n1.id) AS id
+FROM nodes AS n1 
+INNER JOIN nodes AS n2 
+ON (n1.lat = n2.lat AND n1.lon = n2.lon AND n1.id < n2.id)
+GROUP BY n1.lat, n1.lon;
+
+CREATE INDEX ON dedup_nodes (lat, lon);
--- postgis/tables.sql
+++ postgis/tables.sql
@@ -1,6 +1,20 @@
 ﻿DROP TABLE ways,ways_refs,multipols,multipols_refs,nodes;
+
+CREATE TABLE nodes (
+	id BIGINT PRIMARY KEY,
+	lat INTEGER,
+	lon INTEGER,
+	height INTEGER,
+	objtype INTEGER,
+	inside BOOLEAN DEFAULT FALSE,
+	inTunnel BOOLEAN,
+	onBridge BOOLEAN,
+	nodeidx INTEGER,
+	loc GEOMETRY(POINTZ,3065)
+);
+
 CREATE TABLE ways (
-	id INTEGER PRIMARY KEY,
+	id BIGINT PRIMARY KEY,
 --	refs INTEGER[],
 	area BOOLEAN,
 	barrier BOOLEAN,
@@ -15,33 +29,26 @@ CREATE TABLE ways (
 );
 
 CREATE TABLE ways_refs (
-	id INTEGER,
-	ref INTEGER
+	id BIGINT REFERENCES ways(id),
+	ref BIGINT, -- REFERENCES nodes(id),
+	ord INTEGER
 );
 
 CREATE TABLE multipols (
-	id INTEGER PRIMARY KEY,
+	id BIGINT PRIMARY KEY,
 	objtype INTEGER,
 	geom GEOMETRY(MULTIPOLYGON,3065)
 );
 
 CREATE TABLE multipols_refs (
-	id INTEGER,
-	ref INTEGER,
+	id BIGINT REFERENCES multipols(id),
+	ref BIGINT, -- REFERENCES ways(id),
 	role INTEGER);
 
-CREATE TABLE nodes (
-	id INTEGER PRIMARY KEY,
-	lat INTEGER,
-	lon INTEGER,
-	height INTEGER,
-	objtype INTEGER,
-	inside BOOLEAN DEFAULT FALSE,
-	inTunnel BOOLEAN,
-	onBridge BOOLEAN,
-	nodeidx INTEGER,
-	loc GEOMETRY(POINTZ,3065)
-);
+CREATE INDEX ON multipols_refs (ref);
+CREATE INDEX ON ways_refs (ref);
+CREATE INDEX ON nodes (lat, lon);
+
 ALTER TABLE nodes OWNER TO jethro;
 ALTER TABLE ways OWNER TO jethro;
 ALTER TABLE ways_refs OWNER TO jethro;
