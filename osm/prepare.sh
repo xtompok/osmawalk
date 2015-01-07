dfe89f6d494b27914f7e4947773ac463b1642853
Tomas 'Jethro' Pokorny <xtompok@gmail.com>
2015-01-07 22:29:04 +0100

    Each stage can be disabled from command line.

--- osm/prepare.sh
+++ osm/prepare.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
-MINLAT=49.941901
-MINLON=14.224436
-MAXLAT=50.17743
-MAXLON=14.706787
 
 #50.10694/14.44523
 #50.10161/14.45732
@@ -11,14 +7,48 @@ MAXLON=14.706787
 #MAXLAT=50.12
 #MAXLON=14.457
 
+# Prague
+MINLAT=49.941901
+MINLON=14.224436
+MAXLAT=50.17743
+MAXLON=14.706787
+
 # Czech Republic
 #MINLAT=48.5
 #MINLON=12.1
 #MAXLAT=51.1
 #MAXLON=18.9
 
+DOWNLOAD_OSM=1
+DOWNLOAD_HGT=1
+CUT=1
+MERGE=1
 
-if [ $# = 0 ]
+for i
+do
+	case $i in
+		--noosm)
+			DOWNLOAD_OSM=0
+			;;
+		--nohgt)
+			DOWNLOAD_HGT=0
+			;;
+		--nocut)
+			CUT=0
+			;;
+		--nomerge)
+			MERGE=0
+			;;
+		*)
+			echo "$0 [--noosm] [--nohgt] [--nocut] [--nomerge]"
+			exit
+			;;
+	esac
+	shift
+done
+
+
+if [ $DOWNLOAD_OSM = 1 ]
 then
 	posledni=`curl "http://osm.kyblsoft.cz/archiv/last_dates.txt" | grep gz | cut -d"	" -f2`
 	echo "Posledni data z $posledni, stahuji..."
@@ -26,6 +56,17 @@ then
 	curl "http://osm.kyblsoft.cz/archiv/czech_republic-${posledni}.osm.gz" | gunzip > czech_republic.osm
 fi
 
-./osmconvert czech_republic.osm -b=$MINLON,$MINLAT,$MAXLON,$MAXLAT >praha.osm
-./merge-srtm $MINLAT $MINLON $MAXLAT $MAXLON
+if [ $DOWNLOAD_HGT = 1 ]
+then
+	echo "Downloading SRTM data not yet implemented"
+fi
 
+if [ $CUT = 1 ]
+then
+	./osmconvert czech_republic.osm -b=$MINLON,$MINLAT,$MAXLON,$MAXLAT >praha.osm
+fi
+
+if [ $MERGE = 1 ]
+then
+	./merge-srtm $MINLAT $MINLON $MAXLAT $MAXLON
+fi
