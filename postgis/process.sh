deace8ac72825e5adcc9668c90ad636ae01190c8
Jan Plhak <jenda.plhak@gmail.com>
2015-01-16 00:27:55 +0100

    todb.sh, process.sh: --username option added.

--- postgis/process.sh
+++ postgis/process.sh
@@ -1,7 +1,26 @@
-#!/bin/sh
+#!/bin/bash
 
-USERNAME=jethro
+USERNAME=$USER
 DATABASE=osmwalk-prepare
+
+while [[ $# > 0 ]]
+do
+key="$1"
+
+case $key in
+    -h|--help)
+    echo $'-h|--help \n-u|--username <db_username>'
+    exit 0
+    shift
+    ;;
+    -u|--username)
+    shift
+    USERNAME="$1"
+    shift
+    ;;
+esac
+shift
+done
 
 step(){
 	echo
