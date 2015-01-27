c5f498a0188438a76f77bc2379d5dc1c7900b46a
Jan Plhak <jenda.plhak@gmail.com>
2015-01-27 08:45:48 +0100

    finder fail safety added.

--- transfer-finder/finder.py
+++ transfer-finder/finder.py
@@ -16,36 +16,37 @@ outgpx = gpx.GPX("output.gpx")
 search=sp.Popen("./search-transfer",stdin=sp.PIPE,stdout=sp.PIPE)
 print "PID: ",search.pid
 i = 0
-
-for k,v in transfers.iteritems():
-    print i
-    i += 1
-    coords = splitter.match(k)
-    flat = coords.group("flat")
-    flon = coords.group("flon")
-    tlat = coords.group("tlat")
-    tlon = coords.group("tlon")
-    search.stdin.write(flat+" "+flon+" "+tlat+" "+tlon+"\n")
-    search.stdin.flush()
-    line = search.stdout.readline()
-    print line
-    (dist,time)=map(float,line.split(" "))
-    transfers[k]["owlength"]=dist
-    transfers[k]["owtime"]=time
-    transfers[k]["owid"]=i
-    line = search.stdout.readline()
-    while line != "\n": 
-        outgpx.fd.write(line)
+try:
+    for k,v in transfers.iteritems():
+        print i
+        i += 1
+        coords = splitter.match(k)
+        flat = coords.group("flat")
+        flon = coords.group("flon")
+        tlat = coords.group("tlat")
+        tlon = coords.group("tlon")
+        search.stdin.write(flat+" "+flon+" "+tlat+" "+tlon+"\n")
+        search.stdin.flush()
         line = search.stdout.readline()
-    if dist==0:
-        outgpx.addWpt("FAIL_"+str(i)+"_Z",flat,flon,0)
-        outgpx.addWpt("FAIL_"+str(i)+"_K",tlat,tlon,0)
-    else:
-        outgpx.addWpt("OK_"+str(i)+"_Z",flat,flon,0)
-        outgpx.addWpt("OK_"+str(i)+"_K",tlat,tlon,0)
-
-search.kill()
-outgpx.close()
+        print "Search line: {}".format(line)
+        (dist,time)=map(float,line.split(" "))
+        transfers[k]["owlength"]=dist
+        transfers[k]["owtime"]=time
+        transfers[k]["owid"]=i
+        line = search.stdout.readline()
+        while line != "\n": 
+            outgpx.fd.write(line)
+            line = search.stdout.readline()
+        if dist==0:
+            outgpx.addWpt("FAIL_"+str(i)+"_Z",flat,flon,0)
+            outgpx.addWpt("FAIL_"+str(i)+"_K",tlat,tlon,0)
+        else:
+            outgpx.addWpt("OK_"+str(i)+"_Z",flat,flon,0)
+            outgpx.addWpt("OK_"+str(i)+"_K",tlat,tlon,0)
+finally:
+    search.kill()
+    outgpx.close()
+    
 outjson = open("output.json","w")
 json.dump(transfers,outjson,sort_keys=True,indent=4)
 outjson.close()
