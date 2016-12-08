11daeeff3e4dd4f31e5fa1efc7f97a96dda00d55
Tomas Jethro Pokorny <xtompok@gmail.com>
2016-12-08 15:18:06 +0100

    Fixed bug with lacking indexes, now preparation much faster.

--- postgis/direct_candidates6.sql
+++ postgis/direct_candidates6.sql
@@ -10,27 +10,29 @@ CREATE TABLE direct_walk_in_nodes AS (
 );
 
 CREATE INDEX ON direct_walk_in_nodes(id); 
-
-
-DROP TABLE IF EXISTS direct_precandidates;
-CREATE TABLE direct_precandidates AS (
-	SELECT n1.id AS id1, n2.id AS id2, n1.loc AS loc1, n2.loc AS loc2
-	FROM direct_walk_in_nodes AS n1
-	INNER JOIN direct_walk_in_nodes AS n2 ON
-		n1.square1 = n2.square1 OR
-		n1.square2 = n2.square2
-	WHERE n1.id != n2.id
-);
-CREATE INDEX ON direct_precandidates USING GIST(loc1);
-CREATE INDEX ON direct_precandidates USING GIST(loc2);
+CREATE INDEX ON direct_walk_in_nodes(square1); 
+CREATE INDEX ON direct_walk_in_nodes(square2); 
 
 
 DROP TABLE IF EXISTS direct_candidates;
 CREATE TABLE direct_candidates AS (
-   SELECT id1,id2,ST_MakeLine(loc1,loc2) AS geom 
-   FROM direct_precandidates
-   WHERE ST_Distance(loc1,loc2) < 30
+	SELECT n1.id AS id1, n2.id AS id2, ST_MakeLine(n1.loc,n2.loc) AS geom
+	FROM direct_walk_in_nodes AS n1
+	INNER JOIN direct_walk_in_nodes AS n2 ON
+		n1.square1 = n2.square1 OR
+		n1.square2 = n2.square2
+	WHERE n1.id != n2.id AND ST_Distance(n1.loc,n2.loc) < 30
 );
+--CREATE INDEX ON direct_precandidates USING GIST(loc1);
+--CREATE INDEX ON direct_precandidates USING GIST(loc2);
+
+
+--DROP TABLE IF EXISTS direct_candidates;
+--CREATE TABLE direct_candidates AS (
+--   SELECT id1,id2,ST_MakeLine(loc1,loc2) AS geom 
+--   FROM direct_precandidates
+--  WHERE ST_Distance(loc1,loc2) < 30
+--);
 
 --SELECT * FROM make_candidates(444358,5532215,445458,5535Ļ315);
 CREATE INDEX ON direct_candidates USING GIST(geom);
